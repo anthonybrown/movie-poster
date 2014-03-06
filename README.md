75c22e9167ecfe66566087c0c4e602b3a54a0cd1
Tony <anthonybrown@bigmothership.home>
2014-03-06 15:20:21 -0500

    fix js to accomedate new moviedb api

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Movie Poster
+
+### Revisited this project
+
+Since completing this a few years ago, I decided to see if it still worked.
+Alas, it did not, seems like moviedb changed their API since then.
+
+After much Googling and Stackoverflowing, I found a solution.
+For the life of me I could not figure out how to build the URL with the new changes. I tried adding the configure object into the query but that did not work.
+
 playing around with API's playing with the movie database and making
 calls to the API
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(function(){
+	// put the cursor in the form
+	// for usability
 	$('#term').focus();
 
 	$('#term').focus(function(){
@@ -7,41 +9,65 @@ $(document).ready(function(){
 			$('#poster').empty();
 		}
 	});
-	
+
 	var getPoster = function(){
-        
-        var film = $('#term').val();
-			
+
+      var film = $('#term').val();
+
 			if(film == ''){
-			
-				$('#poster').html("<h2 class='loading'>Ha, I didn't forget to add the form validation!<br> Enter something please.</h2>");
-				
+
+				$('#poster').html(
+				  "<h2 class='loading'>Form cannot be left empty<br />Please enter a title.</h2>"
+				  );
+
 			} else {
-		
-				$('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
-			
-				$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/" + film + "?callback=?", function(json) {
-					console.log(json);
-					if (json != "Nothing found."){
-			   			$('#poster').html('<h2 class="loading">Well looky here, I think this is what you are looking for!</h2><img id="thePoster" src=' + json[0].posters[5].image.url + ' />');
+
+
+				$.getJSON("http://api.themoviedb.org/3/configuration/search/movie?api_key=1b3db3672d28f049a0e3f0237eeae403&query=" +
+				    $('#term').val()
+				    + "&callback=?",
+				      function(json) {
+					      console.log(json);
+					      console.log(json.results);
+					  if (json != "Nothing found."){
+			   			$('#poster').html('<h2 class="loading">Here\'s your poster!</h2><img id="thePoster" src=' +
+			   			  json.results.poster_path+' />');
 			   		} else {
-			   			$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/goonies?callback=?", function(json) {
+			   			$.getJSON("http://api.themoviedb.org/3/search/movie?api_key=1b3db3672d28f049a0e3f0237eeae403&query=/goonies&callback=?", function(json) {
 			   				console.log(json);
 			   				$('#poster').html('<h2 class="loading">I\'m afraid nothing was found for that search,Here is the best movie ever though!</h2><img id="thePoster" src=' + json[0].posters[5].image.url + ' />');
 			   			});
 			   		}
 			 	});
-			 	
+
 		 	}
 
         return false;
 	}
-	
+
 	$('#search').click(getPoster);
 	$('#term').keyup(function(event){
 	    if(event.keyCode == 13){
 	        getPoster();
 	    }
 	});
- 	
+
 });
+//				$('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
+//          //var api_key = '1b3db3672d28f049a0e3f0237eeae403';
+//
+//				  $.getJSON('http://api.themoviedb.org/3/search/movie?api_key=1b3db3672d28f049a0e3f0237eeae403&query=' +
+//				      document.getElementById('term').value + '&callback=?', function(json) {
+//				        if (json.total_results > 0) {
+//				          content = '';
+//
+//				          for (i=0; i < json.total_results; i++) {
+//				            url = json.total_results[i].poster_path;
+//				            content += '';
+//				          }
+//
+//				          $('#poster').html(content);
+//				        } else {
+//				          $('#poster').html('<h2>Not found!</h2>');
+//				        }
+//				      });
